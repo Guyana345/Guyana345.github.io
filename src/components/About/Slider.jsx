98147422771c3b8ce0393630fb2987dfb7bb4ac0
Tafadzwa Makuyana <tafadzwa.makuyana@students.stamford.edu>
2018-09-19 21:54:53 +0700

    eploy values for firebase

--- src/components/About/Slider.jsx
+++ src/components/About/Slider.jsx
@@ -7,7 +7,7 @@ class Slider extends Component {
     return (
       <Container className="slider-section ">
         <div className="container">
-          <img src="https://picsum.photos/400" alt="..." class="img-fluid" />
+          <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-154db.appspot.com/o/hero.jpg?alt=media&token=a1513872-b5e5-4b3b-8f0d-2ce44425509d" alt="a picture of me | Tafadzwa Makuyana" class="img-fluid" />
         </div>
       </Container>
     );
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -15,41 +15,55 @@ import "./Portfolio.css";
 
 const BANNER = "https://i.imgur.com/CaKdFMq.jpg";
 
-
 class Portfolio extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    
-    const {projects} = this.props;
+    const { projects } = this.props;
 
     return (
-        <section id="blog">
-          <div className="container">
-            <div className="">
-              <div className="portfolio-section-title text-center">
-                <h2>Projects</h2>
-                <hr className="style-two"/>
-              </div>
+      <section id="portfolio">
+        <div className="container">
+          <div className="">
+            <div className="portfolio-section-title text-center">
+              <h2>Projects</h2>
+              <hr className="style-two" />
             </div>
+          </div>
 
-            <Row>
-              {projects.map((project) => {
-                return (
-                  <Col key={project.name}  xs="12" sm="6" lg="4" className="portfolio-card">
+          <Row>
+            {projects.map(project => {
+              return (
+                <Col
+                  key={project.name}
+                  xs="12"
+                  sm="6"
+                  lg="4"
+                  className="portfolio-card"
+                >
                   <Fragment>
                     <Card>
-                      <CardImg top width="100%" src={project.photoURL} alt="banner" />
+                      <CardImg
+                        top
+                        width="100%"
+                        src={project.photoURL}
+                        alt="banner"
+                      />
                       <CardBody>
                         <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">
-                        <a href={project.website} _blank>{project.name}</a>
+                          <a href={project.website} _blank>
+                            {project.name}
+                          </a>
                         </CardTitle>
-                        <CardSubtitle className="text-secondary mb-3 font-weight-light text-uppercase" style={{ fontSize: "0.8rem" }} >
+                        <CardSubtitle
+                          className="text-secondary mb-3 font-weight-light text-uppercase"
+                          style={{ fontSize: "0.8rem" }}
+                        >
                           {project.category}
                         </CardSubtitle>
-                        
+
                         <CardText
                           className="text-secondary mb-4"
                           style={{ fontSize: "0.75rem" }}
@@ -57,18 +71,15 @@ class Portfolio extends Component {
                           {project.summary}
                         </CardText>
                         <CardText> {project.tech}</CardText>
-                      
                       </CardBody>
                     </Card>
                   </Fragment>
                 </Col>
-                );
-                
-              })}
-
-            </Row>
-          </div>
-        </section>
+              );
+            })}
+          </Row>
+        </div>
+      </section>
     );
   }
 }
